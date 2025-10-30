ed6754ed28c5c014f7517137912beac3eeb9effa
child <child>
2025-10-30 18:46:35 +0800

    c

--- 331-intro-vite/src/App.vue
+++ 331-intro-vite/src/App.vue
@@ -2,12 +2,14 @@
 import { RouterLink, RouterView } from 'vue-router'
 import { useMessageStore } from '@/stores/message'
 import { storeToRefs } from 'pinia'
+import { SpeedInsights } from '@vercel/speed-insights/vue'
 const store = useMessageStore()
 const { message } = storeToRefs(store)
 </script>
 
 <template>
   <div class="text-center font-sans text-gray-700 antialiased">
+      <SpeedInsights />
       <header>
        <div id="flashMessage" class="animate-fade" v-if="message">
          <h4>{{ message }}</h4>
